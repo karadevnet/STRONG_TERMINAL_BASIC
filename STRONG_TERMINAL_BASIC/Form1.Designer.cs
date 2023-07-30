0ca62680a84b1c444cbdd1a6f27976cc3daeaa64
Kaloyan Radev <82239300+karadevnet@users.noreply.github.com>
2023-07-30 17:37:17 +0300

    STRONG TERMINAL BASIC for win10

---
+++ STRONG_TERMINAL_BASIC/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace STRONG_TERMINAL_BASIC
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+      this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.button5 = new System.Windows.Forms.Button();
+      this.button6 = new System.Windows.Forms.Button();
+      this.comboBox1 = new System.Windows.Forms.ComboBox();
+      this.comboBox2 = new System.Windows.Forms.ComboBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.button7 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // serialPort1
+      // 
+      this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+      // 
+      // richTextBox1
+      // 
+      this.richTextBox1.BackColor = System.Drawing.Color.SkyBlue;
+      this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.richTextBox1.Location = new System.Drawing.Point(13, 43);
+      this.richTextBox1.Name = "richTextBox1";
+      this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+      this.richTextBox1.Size = new System.Drawing.Size(496, 547);
+      this.richTextBox1.TabIndex = 0;
+      this.richTextBox1.Text = "";
+      // 
+      // button1
+      // 
+      this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.button1.Location = new System.Drawing.Point(12, 12);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(115, 25);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "GET PORTS";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.button2.Location = new System.Drawing.Point(325, 11);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(115, 25);
+      this.button2.TabIndex = 2;
+      this.button2.Text = "CONNECT";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.BackColor = System.Drawing.Color.SkyBlue;
+      this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.textBox1.Location = new System.Drawing.Point(12, 599);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(497, 22);
+      this.textBox1.TabIndex = 3;
+      // 
+      // button3
+      // 
+      this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.button3.Location = new System.Drawing.Point(522, 11);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(115, 25);
+      this.button3.TabIndex = 4;
+      this.button3.Text = "DISCONNECT";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // button4
+      // 
+      this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.button4.Location = new System.Drawing.Point(446, 11);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(62, 25);
+      this.button4.TabIndex = 5;
+      this.button4.Text = "HEX";
+      this.button4.UseVisualStyleBackColor = true;
+      this.button4.Click += new System.EventHandler(this.button4_Click);
+      // 
+      // button5
+      // 
+      this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.button5.Location = new System.Drawing.Point(522, 544);
+      this.button5.Name = "button5";
+      this.button5.Size = new System.Drawing.Size(115, 25);
+      this.button5.TabIndex = 6;
+      this.button5.Text = "CLEAR";
+      this.button5.UseVisualStyleBackColor = true;
+      this.button5.Click += new System.EventHandler(this.button5_Click);
+      // 
+      // button6
+      // 
+      this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.button6.Location = new System.Drawing.Point(522, 598);
+      this.button6.Name = "button6";
+      this.button6.Size = new System.Drawing.Size(115, 25);
+      this.button6.TabIndex = 8;
+      this.button6.Text = "SEND";
+      this.button6.UseVisualStyleBackColor = true;
+      // 
+      // comboBox1
+      // 
+      this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.comboBox1.FormattingEnabled = true;
+      this.comboBox1.Location = new System.Drawing.Point(133, 12);
+      this.comboBox1.Name = "comboBox1";
+      this.comboBox1.Size = new System.Drawing.Size(90, 24);
+      this.comboBox1.TabIndex = 9;
+      this.comboBox1.Text = "COM1";
+      // 
+      // comboBox2
+      // 
+      this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.comboBox2.FormattingEnabled = true;
+      this.comboBox2.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+      this.comboBox2.Location = new System.Drawing.Point(229, 12);
+      this.comboBox2.Name = "comboBox2";
+      this.comboBox2.Size = new System.Drawing.Size(90, 24);
+      this.comboBox2.TabIndex = 10;
+      this.comboBox2.Text = "9600";
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.label1.Location = new System.Drawing.Point(519, 572);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(39, 18);
+      this.label1.TabIndex = 11;
+      this.label1.Text = "v1.0";
+      // 
+      // button7
+      // 
+      this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.button7.Location = new System.Drawing.Point(522, 44);
+      this.button7.Name = "button7";
+      this.button7.Size = new System.Drawing.Size(115, 25);
+      this.button7.TabIndex = 12;
+      this.button7.Text = "SETTINGS";
+      this.button7.UseVisualStyleBackColor = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.ClientSize = new System.Drawing.Size(649, 633);
+      this.Controls.Add(this.button7);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.comboBox2);
+      this.Controls.Add(this.comboBox1);
+      this.Controls.Add(this.button6);
+      this.Controls.Add(this.button5);
+      this.Controls.Add(this.button4);
+      this.Controls.Add(this.button3);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.richTextBox1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.MaximizeBox = false;
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "STRONG_TERMINAL_BASIC v.1.0";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.IO.Ports.SerialPort serialPort1;
+    private System.Windows.Forms.RichTextBox richTextBox1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button5;
+    private System.Windows.Forms.Button button6;
+    private System.Windows.Forms.ComboBox comboBox1;
+    private System.Windows.Forms.ComboBox comboBox2;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button button7;
+  }
+}
+
---
+++ STRONG_TERMINAL_BASIC/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace STRONG_TERMINAL_BASIC
+{
+  public partial class Form1 : Form
+  {
+    byte[] data_receive = new byte[32];
+    StreamReader streamReader = null;
+    string application_path = Application.ExecutablePath;
+    string path_file_settings = @"settings.txt";
+
+    string DEFAULT_COM_PORT = "DEFAULT_COM_PORT";
+    string DEFAULT_COM_PORT_SPPED = "DEFAULT_COM_PORT_SPPED";
+    string DEFAULT_COM_PORT_MODE = "";
+    string string_ASCII_HEX_MODE = "";
+    bool flag_ASCII_HEX = false;
+    string[] settings_lines = { };
+
+    public Form1()
+    {
+      InitializeComponent();
+      var assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+      var fileVersion = System.Diagnostics.FileVersionInfo.GetVersionInfo(assemblyLocation).FileVersion;
+      string version = "v." + fileVersion.ToString();
+      label1.Text = version.Substring(0,6) + " - 2023";
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      button1.PerformClick();
+      richTextBox1.BackColor = Color.SkyBlue;
+      textBox1.BackColor = Color.SkyBlue;
+      if (File.Exists(path_file_settings))
+      { settings_lines = File.ReadAllLines(@"settings.txt", Encoding.UTF8); }
+      else
+      { richTextBox1.AppendText("\nMISSING FILE FOR SETTINGS !!!\n");
+        richTextBox1.AppendText("create file settings.txt in same folder!!!\n");
+        richTextBox1.AppendText("you can use any TEXT EDITOR\n");
+        richTextBox1.AppendText("like NOTEPAD++ or other text editor\n\n");
+      }
+
+      richTextBox1.AppendText("\n");
+      //richTextBox1.AppendText(setings_lines[0] + "\n");
+      if (File.Exists(path_file_settings))
+      {
+        byte project_count = 0;
+        richTextBox1.AppendText("projects list :\n");
+        //richTextBox1.AppendText("default load is : " + setings_lines[0].Substring(8) + "\n");
+        while (project_count < settings_lines.Length)
+        {
+          if (settings_lines[project_count].StartsWith("PROJECT_NAME"))
+          {
+            richTextBox1.AppendText("project name : " + settings_lines[project_count].Substring(13) + "\n");
+          }
+          project_count++;
+        }
+
+        project_count = 1; // next line after STARTUP_NAME=xxx
+        DEFAULT_COM_PORT_MODE = settings_lines[0].Substring(13);
+        richTextBox1.AppendText("\ndefault load is : " + DEFAULT_COM_PORT_MODE + "\n");
+        //richTextBox1.AppendText("\ndefault load is : " + setings_lines[0].Substring(13) + "\n");
+
+        while (project_count < settings_lines.Length)
+        {
+
+          if (settings_lines[project_count] != "" && settings_lines[project_count].Substring(13) == DEFAULT_COM_PORT_MODE)
+          {
+            richTextBox1.AppendText("project : " + settings_lines[project_count].Substring(13) + "\n");
+
+            richTextBox1.AppendText("serial : " + settings_lines[project_count + 1].Substring(17) + "\n");
+            serialPort1.PortName = settings_lines[project_count + 1].Substring(17);
+
+            richTextBox1.AppendText("speed : " + settings_lines[project_count + 2].Substring(23) + "\n");
+            serialPort1.BaudRate = int.Parse(settings_lines[project_count + 2].Substring(23));
+
+            richTextBox1.AppendText("mode ASCII / HEX : " + settings_lines[project_count + 3].Substring(22) + "\n");
+            if (settings_lines[project_count + 3].Substring(22) == "ASCII")
+            { flag_ASCII_HEX = true; }
+            if (settings_lines[project_count + 3].Substring(22) == "HEX")
+            { flag_ASCII_HEX = false; }
+          }
+          project_count++;
+        }
+        project_count = 0;
+      }
+
+    } // END FORM LOAD
+
+
+    private void button1_Click(object sender, EventArgs e)
+    {    // GET PORTS
+      string[] ports = SerialPort.GetPortNames();
+      richTextBox1.AppendText("rs232 ports found\n\n");
+      comboBox1.Items.Clear();
+
+      foreach (string port in ports)
+      {
+        richTextBox1.AppendText(port + " is active\n");
+        comboBox1.Items.Add(port);
+        comboBox1.SelectedIndex = 0;
+      }
+      var count = comboBox1.Items.Count;
+
+      richTextBox1.AppendText(count + " serial port are active\n");
+      richTextBox1.ScrollToCaret();
+
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {     // CONNECT SERIAL PORT
+      serialPort1.PortName = comboBox1.SelectedItem.ToString();
+
+      if (comboBox2.SelectedIndex == 0)
+      { serialPort1.BaudRate = 9600; }
+      
+      if (comboBox2.SelectedIndex == 1)
+      { serialPort1.BaudRate = 19200; }
+
+      if (comboBox2.SelectedIndex == 2)
+      { serialPort1.BaudRate = 38400; }
+
+      if (comboBox2.SelectedIndex == 3)
+      { serialPort1.BaudRate = 57600; }
+
+      if (comboBox2.SelectedIndex == 4)
+      { serialPort1.BaudRate = 115200; }
+
+       try
+      {
+        if (!(serialPort1.IsOpen))
+        {
+          serialPort1.RtsEnable = true; // IMPORTANT RECEIVE FROM USB ARDUINO/PICO !!!!
+          serialPort1.DtrEnable = true; // IMPORTANT RECEIVE FROM USB ARDUINO/PICO !!!!
+          serialPort1.Open();
+          richTextBox1.AppendText(serialPort1.PortName.ToString() + " is CONNECTED" + "\n");
+          richTextBox1.AppendText(serialPort1.PortName.ToString() + " SPEED = "
+                                          + serialPort1.BaudRate.ToString() + "\n\n");
+        }
+
+        button2.Text = "CONNECTED";
+        button2.BackColor = Color.LightGreen;
+        button2.Enabled = false;
+        button1.Enabled = false;
+        comboBox1.Enabled = false; comboBox2.Enabled = false;
+        richTextBox1.BackColor = Color.LimeGreen;
+        textBox1.BackColor = Color.LimeGreen;
+      }
+      catch (Exception ex)
+      {
+        richTextBox1.AppendText("\nserial port already open >> " + ex.Message + "\n\n");
+        button3.Enabled = false;
+      }
+      richTextBox1.ScrollToCaret();
+    }// END BUTTON CONNECT
+
+    private void button3_Click(object sender, EventArgs e)
+    {   // BUTTON DISCONNECT
+
+      try
+      {
+        serialPort1.DiscardInBuffer();
+        serialPort1.DiscardOutBuffer();
+        serialPort1.Close(); serialPort1.Dispose();
+        button2.Enabled = true;
+        button1.Enabled = true;
+        button2.Text = "CONNECT";
+        button2.BackColor = DefaultBackColor;
+        button2.ForeColor = DefaultForeColor;
+        button2.UseVisualStyleBackColor = true;
+
+        comboBox1.Enabled = true;
+        comboBox2.Enabled = true;
+
+        richTextBox1.BackColor = Color.SkyBlue;
+        textBox1.BackColor = Color.SkyBlue;
+      }
+      catch (Exception ex)
+      {
+        // MessageBox.Show(ex.Message);
+        richTextBox1.AppendText(serialPort1.PortName.ToString() + " is CLOSED" + "\n");
+
+      }
+        var items = comboBox1.Items.Count;
+          while (items > 0)
+          {
+            comboBox1.Items.Clear();
+            items--;
+          }
+        button1.PerformClick();
+
+    }// END BUTTON DISCONNECT
+
+    private void button5_Click(object sender, EventArgs e)
+    {
+      richTextBox1.Clear();
+    }
+
+    private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+      int income_bytes = 0;
+      byte ij = 0;
+      string received_read_string = "";
+      //serialPort1.Read(Init.data_receive, 0, Init.data_receive.Length);
+      // Init.procces_message_receive();
+
+      //=========== EDITED ROUTINE FOR AVOID MISSING FIRST BYTE =========
+
+      //if you need to count and print or check number received bytes
+      // ENABLE NEXT LINES !!!
+      while (serialPort1.BytesToRead > 0) // For example
+      {
+        income_bytes++; // IMPORTANT COUNT++ TO GET MESSAGE
+        serialPort1.Read(data_receive, 0, data_receive.Length);
+      }
+
+
+      if (flag_ASCII_HEX == true) // ASCII MODE ACTIVE
+      {
+        received_read_string = serialPort1.ReadLine();
+        richTextBox1.AppendText("receive >>\n");
+        richTextBox1.AppendText(received_read_string + "\n\n");
+        //richTextBox1.AppendText("\n\n");
+        richTextBox1.ScrollToCaret();
+      }
+      else
+      {
+        richTextBox1.AppendText("receive >>\n");
+        while (ij < data_receive.Length)
+          {
+            if (data_receive[ij] < 16 && data_receive[ij] > 0)
+            {
+            richTextBox1.AppendText("0" + data_receive[ij].ToString("X") + " ");
+            //received_read_string += "0" + data_receive[ij].ToString("X") + " ";
+            }
+            else if (data_receive[ij] > 15)
+            {
+            richTextBox1.AppendText(data_receive[ij].ToString("X") + " ");
+            //received_read_string += data_receive[ij].ToString("X") + " ";
+            }
+         
+          ij++;
+          }
+		  // use for PREOTEUS SIMULATION
+		  // for virtual ports
+        System.Threading.Thread.Sleep(100);
+       // richTextBox1.AppendText(received_read_string);
+
+        richTextBox1.AppendText("\n");
+
+        ij = 0;
+        while (ij < data_receive.Length)
+        {
+          data_receive[ij] = 0;
+          ij++;
+        }
+        ij = 0;
+
+       // richTextBox1.AppendText("\n\n");
+        richTextBox1.ScrollToCaret();
+        //received_read_string = "";
+      //  income_bytes = 0;
+
+
+      }
+    }//=========== EDITED ROUTINE FOR AVOID MISSING FIRST BYTE =========
+
+    private void button4_Click(object sender, EventArgs e)
+    {
+      if (flag_ASCII_HEX == false)
+      { flag_ASCII_HEX = true;
+        richTextBox1.AppendText("\nASCII mode active\n");
+        richTextBox1.ScrollToCaret();
+        button4.Text = "ASCII";
+      }
+      else
+      if (flag_ASCII_HEX == true)
+      { flag_ASCII_HEX = false;
+        richTextBox1.AppendText("\nHEX mode active\n");
+        richTextBox1.ScrollToCaret();
+        button4.Text = "HEX";
+      }
+
+      //if (setings_lines[project_count + 3].Substring(22) == "ASCII")
+      //{ flag_ASCII_HEX = true; }
+      //if (setings_lines[project_count + 3].Substring(22) == "HEX")
+     // { flag_ASCII_HEX = false; }
+    }
+
+    // ============== NEXT CODE PLACE HERE ================
+
+
+  }
+}
